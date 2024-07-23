8f01a1c464b152bd840e9680a4024cca6f30caf7
mRud3r <mrud.priv@gmail.com>
2024-07-23 18:37:08 +0200

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Cart from "./components/Cart.jsx"
 import Product from "./components/Product.jsx"
 
 function App() {
 
   return (
     <>
+    <Cart />
     <Product />
     </>
   )
---
+++ src/components/Cart.jsx
@@ -0,0 +1,6 @@
+export default function Cart() {
+    return (
+        <div className="fixed end-0 h-screen w-56 bg-slate-200">
+        </div>
+    );
+}
