ca52f2589612915fedeb3c296bfab04ab65769f4
mRud3r <mrud.priv@gmail.com>
2024-08-06 17:28:20 +0200

    small update for Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -9,18 +9,27 @@ export default function Cart({isOpen, onClose, cartItems, removeFromCart, addToC
   }, 0);
 
     return (
-        <div className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform ${
-            isOpen ? 'translate-x-0' : 'translate-x-full'
-        } transition-transform duration-300 ease-in-out`}>
-            <button onClick={onClose} >close</button>
-            <ul>
-        {cartArray.map((item) => (
-          <CartItem key={item.id} item={item} onRemove={removeFromCart} onAdd={addToCart} />
-        ))}
-      </ul>
-      <div className="p-4 border-t">
-        <h3>Total Price: ${totalPrice}</h3>
-      </div>
+      <div className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform ${
+        isOpen ? 'translate-x-0' : 'translate-x-full'
+      } transition-transform duration-300 ease-in-out`}>
+      <button onClick={onClose}>Close</button>
+
+      {cartArray.length === 0 ? (
+        <div className="p-4">
+          <h3>No items in the cart!</h3>
         </div>
-    );
+      ) : (
+        <>
+          <ul>
+            {cartArray.map((item) => (
+              <CartItem key={item.id} item={item} onRemove={removeFromCart} onAdd={addToCart} />
+            ))}
+          </ul>
+          <div className="p-4 border-t">
+            <h3>Total Price: ${totalPrice}</h3>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
